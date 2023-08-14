938952e8f2be75b410cac67fc3ab61f69e277430
fox <fox@192>
2023-08-14 09:59:49 -0400

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-if __name__=="__main__":
-    """print Print the number of and list of arguments"""
+# 2-args.py
+# Brennan D Baraban <375@holbertonschool.com>
 
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
-    count=len(sys.argv) -1
-    if count== 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif count == 1 :
-        print ("1 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{}arguments:".format(count))
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-        
--- 0x02-python-import_modules/variable_load_5.py
+++ 0x02-python-import_modules/variable_load_5.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 a = 98
-
 """Simple variable
 """
